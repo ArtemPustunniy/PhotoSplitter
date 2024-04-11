ec3ddf0604813c8cde33640a3b59c919699926cb
ArtemPustunniy <144416748+ArtemPustunniy@users.noreply.github.com>
2024-04-12 00:04:55 +0300

    Update settings.py

--- facedetect/facedetect/settings.py
+++ facedetect/facedetect/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4gb_@=dm(gmm6%e$$4)+)tk51hjrc9v7t06qm_jrk0ekk#sa(a'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
